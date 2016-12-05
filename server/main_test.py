f87d101460ba133886c835803d6c32a28558536b
Caleb Huitt <development-caleb@huitt.org>
2016-12-04 21:57:46 -0600

    Test that money is sent on valid login.

--- server/main_test.py
+++ server/main_test.py
@@ -15,6 +15,7 @@ class DbStub():
         self.pass_auth = False
         self.auth_user = None
         self.auth_pass = None
+        self.user_money = None
 
     def auth(self, user, passwd):
         self.auth_user = user
@@ -22,7 +23,7 @@ class DbStub():
         return self.pass_auth
 
     def money(self, param):
-        return None
+        return self.user_money
 
 
 class GameStub():
@@ -44,6 +45,19 @@ class HandleAuthTests(unittest.TestCase):
         main.handle_auth(command='login', params=params, game=game, db=db, comms=comms)
 
         self.assertTrue(game.began)
+
+    def test_returns_money_on_valid_login(self):
+        params = ['user', 'passwd']
+        comms = CommsStub()
+        db = DbStub()
+        game = GameStub()
+
+        db.pass_auth = True
+        db.user_money = 50
+        main.handle_auth(command='login', params=params, game=game, db=db, comms=comms)
+
+        self.assertTrue('50' in comms.sent_message)
+
 
 
 if __name__ == '__main__':
