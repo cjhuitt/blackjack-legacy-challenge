a1a28c7cea6b31a85b41c61ff9a9b1d8f8500237
Caleb Huitt <development-caleb@huitt.org>
2016-12-04 21:36:36 -0600

    Extract auth handling into it's own function

--- server/main.py
+++ server/main.py
@@ -6,22 +6,27 @@ from game import Game
 import sys
 
 
+def handle_auth(command, params, game, db, comms):
+    if command == 'login':
+        if db.auth(*params):
+            money = db.money(params[0])
+            comms.send('auth {0}'.format(money))
+            game.begin()
+        else:
+            comms.send('auth_fail')
+
+
 def main(argv):
     c = Comm()
     g = Game()
-    while(True):
+    while (True):
         c.wait()
         message = Comm().receive()
         if message is not None:
             name, *params = message.split(' ')
-            if name == 'login':
-                if Db().auth(*params):
-                    money = Db().money(params[0])
-                    Comm().send('auth {0}'.format(money))
-                    g.begin()
-                else:
-                    Comm().send('auth_fail')
+            handle_auth(command=name, params=params, game=g, db=Db(), comms=Comm())
     return 0
+
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
